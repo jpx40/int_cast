201c092b88a164be93ad1aa79f895e4733fd8102
jonas <jonaspeitz@proton.me>
2024-12-22 13:20:10 +0100

    patchet bug

--- src/lib.rs
+++ src/lib.rs
@@ -3,83 +3,83 @@ use std::ops::Shl;
 #[inline(always)]
 pub fn i32<T: Sized>(i: T) -> i32
 where
-    T: Shl<i32> ,
+ i32: From<T>
 {
     i.into()
 }
 #[inline(always)]
 pub fn i64<T: Sized>(i: T) -> i64
 where
-    T: Shl<i64>,
+     i64: From<T>
 {
     i.into()
 }
 #[inline(always)]
 pub fn i16<T: Sized>(i: T) -> i16
 where
-    T: Shl<i16>,
+ i16: From<T>
 {
     i.into()
 }
 #[inline(always)]
 pub fn f32<T: Sized>(i: T) -> f32
 where
-    T: Shl<f32>,
+  if32: From<T>
 {
     i.into()
 }
 #[inline(always)]
 pub fn f64<T: Sized>(i: T) -> f64
 where
-    T: Shl<f64>,
+ f64: From<T>
 {
     i.into()
 }
 #[inline(always)]
 pub fn i128<T: Sized>(i: T) -> i128
 where
-    T: Shl<i128>,
+   i128: From<T>
 {
     i.into()
 }
 #[inline(always)]
 pub fn u128<T: Sized>(i: T) -> u128
 where
-    T: Shl<u128>,
+ u128: From<T>
 {
     i.into()
 }
 #[inline(always)]
 pub fn u32<T: Sized>(i: T) -> u32
-where
-    T: Shl<u32>,
+where     u32: From<T>,
+   
 {
     i.into()
 }
 #[inline(always)]
 pub fn u16<T: Sized>(i: T) -> u16
 where
-    T: Shl<u16>,
+    u16: From<T>,
 {
     i.into()
 }
 pub fn u8<T: Sized>(i: T) -> u8
 where
-    T: Shl<u8>,
+    u8: From<T>,
 {
     i.into()
 }
 #[inline(always)]
 pub fn i8<T: Sized>(i: T) -> i8
 where
-    T: Shl<i8>,
+    i8: From<T>,
 {
     i.into()
 }
 #[inline(always)]
 pub fn u64<T: Sized>(i: T) -> u64
 where
-    T: Shl<u64>,
+    u64: From<T>,
 {
     i.into()
 }
